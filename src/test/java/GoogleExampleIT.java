28aa4f62b66bb360eb90e0f19276532fff1e6851
wppriestley <wppriestley@gmail.com>
2018-01-11 17:58:26 -0500

    Fixed failing tests

--- src/test/java/GoogleExampleIT.java
+++ src/test/java/GoogleExampleIT.java
@@ -1,20 +1,22 @@
-/*
- * Requirement:
- * 
- *  1. please fix all the bug in this file 
- *  2. please run it
- *  3. sent your test result and the fixed the file to us
- * 
- * 
- * */
-
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.util.logging.Logger;
 
 @Test(groups={"localTest"})
 public class GoogleExampleIT {
-	private static final Logger LOGGER = LogManager.getLogger(GoogleExampleIT.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(GoogleExampleIT.class.getName());
 	private WebDriver driver;
-	@FindBy(css = "[name='q']")
-	private WebElement searchBar;
+	private  @FindBy(css = "[name='q']") WebElement searchBar;
+
 
 	@Test
 	public void googleCheeseExample() throws Exception {
@@ -22,7 +24,7 @@ public class GoogleExampleIT {
 		searchBar.sendKeys("Cheese!");
 		searchBar.sendKeys(Keys.ENTER);
 		LOGGER.info("Page title is: " + driver.getTitle());
-		Assert.assertTrue(driver.getTitle().equals("Google"));
+		Assert.assertTrue(driver.getTitle().equals("Cheese! - Google Search"));
 	}
 
 	@Test(enabled = true)
@@ -30,14 +32,14 @@ public class GoogleExampleIT {
 		searchBar.clear();
 		searchBar.sendKeys("Milk!");
 		searchBar.sendKeys(Keys.ENTER);
-		LOGGER.info("Page title is: {}", driver.getTitle());
-		Assert.assertTrue(driver.getTitle().equals("Google"));
+		LOGGER.info("Page title is: " + driver.getTitle());
+		System.out.println(driver.getTitle());
+		Assert.assertTrue(driver.getTitle().equals("Milk! - Google Search"));
 	}
 
 	@BeforeMethod
 	public void setUp() {
-		
-		driver = new new FirefoxDriver();
+		driver = new FirefoxDriver();
 		PageFactory.initElements(new AjaxElementLocatorFactory(driver, 60), this);
 		driver.get("http://www.google.com");
 	}
